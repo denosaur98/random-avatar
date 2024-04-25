38d5bb3bf96c61e9226cae17030105c5ec300c35
denosaur98 <denischurchill1998@gmail.com>
2024-04-26 00:13:05 +0300

    fix router + favicon

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createWebHistory, createRouter } from 'vue-router'
+import { createWebHashHistory, createRouter } from 'vue-router'
 
 import AvatarGenerate from '../components/AvatarGenerate.vue'
 import AvatarFavorites from '../components/AvatarFavorites.vue'
 
 export default createRouter({
-	history: createWebHistory(),
+	history: createWebHashHistory(),
 	routes: [
 		{ path: '/', component: AvatarGenerate, alias: '/' },
 		{ path: '/favorites', component: AvatarFavorites },
